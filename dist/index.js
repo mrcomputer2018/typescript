dd1b7868672bfc15c9e8a9524ee6e19a36b6dbad
Marcelo Antonio <marcelo.ribeiro@proeja.com>
2024-01-18 15:24:44 -0300

    Add new variables and constants

--- dist/index.js
+++ dist/index.js
@@ -5,3 +5,22 @@ const isAdult = true;
 console.log(age);
 console.log(firstName);
 console.log(isAdult);
+const ages = [33, 28, 11];
+const ids = ['abc', 'def', 'ghi'];
+const boleans = [true, false, true];
+let person = ['John', 33, true];
+let employee = [
+    ['John', 33],
+    ['Mary', 22],
+    ['Bill', 44]
+];
+const productId = '22';
+var Colors;
+(function (Colors) {
+    Colors["white"] = "#fff";
+    Colors["black"] = "#000";
+})(Colors || (Colors = {}));
+const white = Colors.white;
+const black = Colors.black;
+console.log(white);
+console.log(black);
